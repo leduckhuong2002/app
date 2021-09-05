0e598f2a61348540212451773a837f9f0718cd52
@khuong <leduckhuong2002@gmail.com>
2021-09-05 13:36:49 +0700

    add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import dotenv from 'dotenv';
+import handlebars from 'express-handlebars';
+
+import helperHandlebars from './helpers/handlebars';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT||3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+
+app.set('views', path.join(__dirname, 'resources','views'));
+app.engine('hbs', handlebars(helperHandlebars));
+app.set('view engine', 'hbs');
 app.use('/', (req, res) =>{
-    res.send('I Love You');
+    res.render('home');
 });
 
 app.listen(port, () => console.log(`App listen in http://localhost:${port}`));
