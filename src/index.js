43875bf24d4739f1deae252e1ad1b8dee8881ae5
@khuong <leduckhuong2002@gmail.com>
2021-09-05 09:05:43 +0700

    add begin file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT||3000;
+
+app.use('/', (req, res) =>{
+    res.send('oce');
+});
+
+app.listen(port, () => console.log(`App listen in http://localhost:${port}`));
