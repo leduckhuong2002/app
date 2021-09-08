697ff2226528ebfb2b52025981233d9b44f07ccd
@khuong <leduckhuong2002@gmail.com>
2021-09-08 08:36:01 +0700

    success

--- src/app/controllers/Login.js
+++ src/app/controllers/Login.js
@@ -1,11 +1,12 @@
-// import User from "../models/User";
+import User from "../models/User.js";
+
 class Login{
     auto(req, res, next){
         res.render('login');
     };
-    // post(req, res, next){
-    //     const user = new User(req.body);
-    //     user.save().then(()=> res.redirect('/')).catch(next);
-    // };
+    post(req, res, next){
+        const user = new User(req.body);
+        user.save().then(()=> res.redirect('/')).catch(next);
+    };
 };
 export default new Login();
--- src/routers/sub-routers/login.js
+++ src/routers/sub-routers/login.js
@@ -2,5 +2,5 @@ import express from 'express';
 const router = express.Router();
 import login from '../../app/controllers/Login.js'
 router.get('/auto', login.auto);
-// router.post('/post', login.post);
+router.post('/post', login.post);
 export default router;
