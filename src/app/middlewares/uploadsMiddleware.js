94c91bf9739959e142b4fc8012324d9fd58010bf
@khuong <leduckhuong2002@gmail.com>
2021-09-09 11:20:40 +0700

    add

--- src/app/middlewares/uploadsMiddleware.js
+++ src/app/middlewares/uploadsMiddleware.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import multer  from 'multer';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const upload = multer({ dest: path.resolve(__dirname, '../../../public/uploads') });
+const upload = multer({ dest: path.resolve(__dirname, '../../../../public/uploads') });
 export default {
     upload,
     opUpload : upload.fields([{ name: 'imgProduct' }, { name: 'listImg', maxCount: 8 }])
